d41dad18378102f749c87351a8cff424de965414
Choi Junyoung <nueeeng@gmail.com>
2023-07-14 23:54:04 +0900

    Utterance test

--- src/components/post-content/index.js
+++ src/components/post-content/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./index.scss";
+import Utterances from "../Utterances";
 
 const PostContent = ({ content }) => {
   return (
-    <div
-      className="body-1 postContent"
-      dangerouslySetInnerHTML={{ __html: content }}
-    />
+    <>
+      <div
+        className="body-1 postContent"
+        dangerouslySetInnerHTML={{ __html: content }}
+      />
+      <Utterances />
+    </>
   );
 };
 export default PostContent;
